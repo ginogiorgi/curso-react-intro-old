1d67658f9b3399a86c7976cb0731b4e068e8f6b5
ginogiorgi <ginorubengiorgi@gmail.com>
2023-06-30 08:21:40 -0300

    Clase 20 Curso de React.js

--- src/App/index.js
+++ src/App/index.js
@@ -2,16 +2,19 @@ import { useLocalStorage } from "./useLocalStorage"
 import { AppUI } from "./AppUI"
 import React from 'react';
 
-const defaultTodos = [
-  {text: "Cortar cebolla", completed: false},
-  {text: "Ir a la comicon", completed: true},
-  {text: "Cortar pasto", completed: false},
-  {text: "Hacer tareas de la UTN", completed: true},
-  {text: "Mirar cualquier película que no sea el señor de los anillos!", completed: false},
-];
+// localStorage.removeItem('TODOS_V1');
+// const defaultTodos = [
+//   {text: "Cortar cebolla", completed: false},
+//   {text: "Ir a la comicon", completed: true},
+//   {text: "Cortar pasto", completed: false},
+//   {text: "Hacer tareas de la UTN", completed: true},
+//   {text: "Mirar cualquier película que no sea el señor de los anillos!", completed: false},
+// ];
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
+
 
 function App() {
-  const {item: todos, saveItem: saveTodos, loading, error} = useLocalStorage("TODOS_V1", defaultTodos);
+  const {item: todos, saveItem: saveTodos, loading, error} = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
   const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
--- src/App/useLocalStorage.js
+++ src/App/useLocalStorage.js
@@ -3,19 +3,29 @@ import React from 'react';
 function useLocalStorage(itemName, initialValue) {
     const [item, setItem] = React.useState(initialValue);
     const [loading, setLoading] = React.useState(true);
-    const [error, setError] = React.useState(true);
+    const [error, setError] = React.useState(false);
   
     React.useEffect(() => {
-      const localStorageItem = localStorage.getItem(itemName);
-      let parsedItem;
-      
-      if (localStorageItem) {
-        parsedItem = JSON.parse(localStorageItem);
-      } else {
-        localStorage.setItem("TODOS_V1", JSON.stringify(initialValue));
-        parsedItem = initialValue;
-      };
-    });
+      setTimeout(() => {
+        try{
+          const localStorageItem = localStorage.getItem(itemName);
+          let parsedItem;
+          
+          if (localStorageItem) {
+            parsedItem = JSON.parse(localStorageItem);
+            setItem(parsedItem);
+          } else {
+            localStorage.setItem("TODOS_V1", JSON.stringify(initialValue));
+            parsedItem = initialValue;
+          };
+    
+          setLoading(false);
+        } catch(error) {
+          setLoading(false);
+          setError(true);
+        };
+      }, 2000)
+    }, [initialValue, itemName]);
   
     const saveItem = (newItem) => {
       localStorage.setItem("TODOS_V1", JSON.stringify(newItem));
